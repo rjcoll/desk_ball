a5e2af638dd8d8932ac734b34130776314b59f58
Rob Collier <robjcollier@gmail.com>
2015-12-25 15:32:41 +0000

    Changed player service to use array

--- js/services/player.js
+++ js/services/player.js
@@ -1,8 +1,7 @@
 app.service('playerService', function() {
-  var playerList = 'Test';
-
   this.value0 = 'Will'
   this.value1 = 'Ben'
   this.value2 = 'Rob'
 
+  this.p= ['Will', 'Ben', 'Rob']
 });
